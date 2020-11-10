01b36ed24c8f21193a1e8580c6e147ee37fb6204
DESKTOP-RRM7P8C\Khami <napakamee_n@cmu.ac.th>
2020-11-10 16:56:14 +0700

    add speed meter

--- Assets/SpeedDisplay.cs
+++ Assets/SpeedDisplay.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SpeedDisplay : MonoBehaviour
 {
+    private float speed;
+    public Text speedTxt;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speed = (rb.velocity.magnitude) * 10f;
+
+        speedTxt.text = "Speed: " + speed.ToString("F2");
     }
 }
