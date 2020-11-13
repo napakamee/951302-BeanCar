06054e8ddb1f51984f4431944f28379c9a0e1508
DESKTOP-RRM7P8C\Khami <napakamee_n@cmu.ac.th>
2020-11-13 23:38:59 +0700

    add winZone

---
+++ Assets/Scripts/WinZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerScript Type = other.gameObject.GetComponent<PlayerScript>();
+        if (other.gameObject.CompareTag("Player"))
+        {
+            switch (Type.playerType)
+            {
+                case PlayerType.Player1:
+                     GameManager.playerWin = 1; break;
+                case PlayerType.Player2:
+                     GameManager.playerWin = 2; break;
+            }
+        }
+
+    }
+}
