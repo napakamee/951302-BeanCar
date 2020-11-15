1bf38d6fb87c7b2bc7cfbefe4bd876169c770a6f
noomnoose <badman410305@gmail.com>
2020-11-15 11:28:14 +0700

    add wall and modfy map

---
+++ Assets/Scripts/AddForce.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    public float damageRate;
+    public float pushBackForce;
+
+    float nextDamage;
+
+    bool playerInRange = false;
+
+    GameObject thePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextDamage = Time.time;
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange) Attack();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+    void Attack()
+    {
+        if (nextDamage <= Time.time)
+        {
+
+            nextDamage = Time.time + damageRate;
+
+            pushBack(thePlayer.transform);
+        }
+    }
+    void pushBack(Transform pushedObjecct)
+    {
+        Vector3 pushDirection = new Vector3(0, (pushedObjecct.position.y - transform.position.y), 0).normalized;
+        pushDirection *= pushBackForce;
+
+        Rigidbody pushedRB = pushedObjecct.GetComponent<Rigidbody>();
+        pushedRB.velocity = Vector3.zero;
+        pushedRB.AddForce(pushDirection, ForceMode.Impulse);
+    }
+}
